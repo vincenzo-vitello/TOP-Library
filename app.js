c85303a6a268fefdc5ab56ae7a97d53323494c57
Vincenzo Vitello <vincenzo.vitello@icloud.com>
2024-12-18 17:07:53 +0100

    added theme mode switcher

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const body = document.querySelector("body");
 const filtersIcon = document.getElementById("filters");
+const themeToggler = document.getElementById("theme_toggler");
+
 const setColorScheme = () => {
-  if (body.getAttribute("data-theme") === "dark") {
+  const currentTheme = body.getAttribute("data-theme");
+  if (currentTheme === "dark") {
+    body.setAttribute("data-theme", "light");
+
+    filtersIcon.setAttribute("src", "./assets/filters-light.svg");
+    themeToggler.setAttribute("src", "./assets/light-mode.svg");
+  } else {
+    body.setAttribute("data-theme", "dark");
     filtersIcon.setAttribute("src", "./assets/filters-dark.svg");
+    themeToggler.setAttribute("src", "./assets/dark-mode.svg");
   }
 };
-document.addEventListener("DOMContentLoaded", () => {
-  setColorScheme();
-});
+themeToggler.onclick = () => setColorScheme();
